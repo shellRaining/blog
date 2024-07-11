9c44b8e56a46bda178b599fb056b96b6003cc865
shellRaining <shellRaining@gmail.com>
2024-07-11 22:13:14 +0800

    add unocss support

---
+++ uno.config.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-restricted-imports
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      cdn: 'https://esm.sh/',
+    }),
+  ],
+})
+
