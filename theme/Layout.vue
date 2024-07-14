f28a53cef6f44ea73b9d6aa94c7e0ef8e92afbad
shellRaining <shellRaining@gmail.com>
2024-07-14 23:48:44 +0800

    feat: add toggle color mode animation

--- theme/Layout.vue
+++ theme/Layout.vue
@@ -3,8 +3,44 @@ import DefaultTheme from "vitepress/theme";
 import Page from "./Home/Home.vue";
 import Title from "./Doc/Title.vue";
 import Gallery from "./Viewer/Gallery.vue";
+import { useData } from "vitepress";
+import { nextTick, provide } from "vue";
 
 const { Layout } = DefaultTheme;
+
+const { isDark } = useData();
+
+const enableTransitions = () =>
+  "startViewTransition" in document &&
+  window.matchMedia("(prefers-reduced-motion: no-preference)").matches;
+
+provide("toggle-appearance", async ({ clientX: x, clientY: y }: MouseEvent) => {
+  if (!enableTransitions()) {
+    isDark.value = !isDark.value;
+    return;
+  }
+
+  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y),
+    )}px at ${x}px ${y}px)`,
+  ];
+
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value;
+    await nextTick();
+  }).ready;
+  document.documentElement.animate(
+    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+    {
+      duration: 500,
+      easing: "ease-in",
+      pseudoElement: `::view-transition-${isDark.value ? "old" : "new"}(root)`,
+    },
+  );
+});
 </script>
 
 <template>
@@ -17,3 +53,30 @@ const { Layout } = DefaultTheme;
     <template #home-features-after><Page /></template>
   </Layout>
 </template>
+
+<!-- don't use scope -->
+<style>
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root),
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root),
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
+
+.VPSwitchAppearance {
+  width: 22px !important;
+}
+
+.VPSwitchAppearance .check {
+  transform: none !important;
+}
+</style>
